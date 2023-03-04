f3b11f2c3654f4dd1e5ff3a38f512e5975bcfd07
Jose Luis Conde Diez <joseluis.condediez@gmail.com>
2023-03-04 14:41:00 +0100

    Update ejercicio-3.js

--- sprint-1/ejercicio-3.js
+++ sprint-1/ejercicio-3.js
@@ -54,6 +54,51 @@ const callback = (param) => {
 
 miFuncion(10, callback);
 
+//Nivel 2/ Ejercicio 1
+/*Donats els objectes employees i salaries, crea una arrow function getEmployee() 
+que retorni una Promise efectuant la cerca en l'objecte pel seu id.*/
+
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+}, {
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+
+const getEmployee = (id) => {
+    return new Promise((resolve, reject) => {
+        if (id > employees.length || id <= 0) {
+            reject(new Error('No existe el empleado con ese Id'));
+        }
+        resolve(buscarId(id));
+    });
+}
 
 
+const buscarId = (id) => {
+    employees.forEach(o => {
+        if (o.id === id) {
+            console.log(o.name);
+        }
+    });;
+}
+
+getEmployee(1)
+    .then((employees))
+    .catch(error => console.error(error.message));
 
