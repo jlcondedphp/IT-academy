ad7f0726b57ced6ed399d9dd8d3a6070ddf91144
joseluis <joseluis.condediez@gmail.com>
2023-03-08 13:14:40 +0100

    Se acaba el ejercicio 1.3

--- sprint-1/ejercicio-3.js
+++ sprint-1/ejercicio-3.js
@@ -1,62 +1,52 @@
-//Nivel 1/ Ejercicio 1
+//Level 1/ Exercise 1
 
-/*Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() 
-que rep. Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge 
-diferent depenent de si la Promise es resol o no.
-
-//peliculas
-peliculas = [
+ films= [
     {
         id: 1,
-        titulo: 'Pelicula 1',
-        any: 2015
+        title: 'Film 1',
+        year: 2015
     }, {
         id: 2,
-        titulo: 'Pelicula 2',
-        any: 2020
+        title: 'Film 2',
+        year: 2020
     }
 ];
 
-//Comprobacion si no hay datos en el array
-//peliculas = [];
+//Checking empty films
+//films= [];
 
-
-const getPelis = () => {
+const getFilms = () => {
     return new Promise((resolve, reject) => {
-        if (peliculas.length === 0) {
-            reject(new Error('No existen peliculas'));
+        if (films.length === 0) {
+            reject(new Error('There are no movies'));
         }
-        resolve(peliculas);
+        resolve(films);
     });
 }
 
-//En caso que no existan peliculas, devolvemos el error!!
-getPelis()
-    .then((peliculas) => console.log(peliculas))
+getFilms()
+    .then((films) => console.log(films))
+    //In case of error return the error
     .catch(error => console.error(error.message));
 
-//Nivel 1/ Ejercicio 2
-/*Crea una arrow function que rebi un paràmetre i una funció callback i li passi a la funció un missatge o un 
-altre (que s'imprimirà per consola) en funció del paràmetre rebut.
 
+//Level 1/ Exercise 2
 
-const miFuncion = (param, callback) => {
-    console.log(`Estamos dentro de miFuncion con un parametro con valor ${param}`);
+const miFunction = (param, callback) => {
+    console.log(`We are within miFunction with a parameter value ${param}`);
     callback(param);
 }
 const callback = (param) => {
     if (param > 0) {
-        console.log('Hay un valor positivo')
+        console.log('There is a positive value')
     } else {
-        console.log('Hay un valor negativo');
+        console.log('There is a negative value');
     }
 }
 
-miFuncion(10, callback);
+miFunction(-5, callback);
 
-//Nivel 2/ Ejercicio 1
-/*Donats els objectes employees i salaries, crea una arrow function getEmployee() 
-que retorni una Promise efectuant la cerca en l'objecte pel seu id.*/
+//Level 2/ Exercise 1
 
 let employees = [{
     id: 1,
@@ -81,70 +71,78 @@ let salaries = [{
 }];
 
 
-
 const getEmployee = (id) => {
     return new Promise((resolve, reject) => {
-        const empleado = employees.find((empleado) => empleado.id === id);
-        if (!empleado) {
-            reject(new Error(`El empleado con ese id no existe`));
+        const employee = employees.find( employee => employee.id == id);
+        if (employee) {
+            resolve(employee.name);            
         } else {
-            resolve(empleado.name);
+            reject('The employee with that id does not exists');            
         }
     });
 }
 
-getEmployee(1)
-    .then(encontrado => console.log(`El id encontrado pertenece al empleado ${encontrado}`))
-    .catch(error => console.error(error.message));
+getEmployee(2)
+    .then(employee => console.log(employee))
+    .catch(error => console.error(error));
 
 
-//Nivel 2/ Ejercicio 2
-const getSalary = (object) => {
+
+//Level 2/ Exercise 2
+const getSalary = (employee) => {
     return new Promise((resolve, reject) => {
 
-        salario = salaries.find((salario) => salario.id === object.id);
+        const salary = salaries.find( salary => salary.id == employee.id);
 
-        if (salario !== undefined) {
-            resolve(`El salario del empleado es ${salario.salary}`);
+        if (salary) {
+            resolve(salary.salary);                    
         }
         else {
-            reject(new Error('No existe el id'));
+            reject('The employee with that id does not exists');           
         }
     });
 }
 
 getSalary(employees[1])
-    .then(salario => console.log(salario))
-    .catch(error => console.error(error.message));
+    .then(salary => console.log(salary))
+    .catch(error => console.error(error));
 
-//Nivel 2/ Ejercicio 3
 
-const getEmployee2 = (id) => {
-    return new Promise((resolve, reject) => {
-        const empleado = employees.find(empleado => empleado.id === id);
-        if (empleado) {
-            resolve(empleado.name);
-        } else {
-            reject(new Error('No existe el id'));
+//Level 2/ Exercise 3
+
+const getEmployee2 = (id) =>{
+    return new Promise((resolve,reject) =>{
+        const employee = employees.find(employee => employee.id == id);
+        if(employee){
+            resolve(employee);
+        }else{
+            reject("No existe ese id");
         }
     });
 }
 
-const getSalary2 = (empleado) => {
-    return new Promise((resolve, reject) => {
-        const salario = salaries.find(salario => salario.id === empleado.id);
-        if (salario) {
+const getSalary2 = (employee) =>{
+    return new Promise((resolve,reject) =>{
+        const salary = salaries.find(salary => salary.id == employee.id);
+        if(salary){
             resolve({
-                Nombre: empleado.name,
-                Salario: salario.salary
+                Nombre: employee.name,
+                Salario: salary.salary
             });
-        } else {
+        }else{
             reject('No existe el id');
         }
     });
 }
 
+getEmployee2(1)
+        .then(employee => getSalary2(employee))
+        .then(employeeSalary => console.log(employeeSalary))
+        
 
-//Nivel 3/ Ejercicio 1
 
+//Level 3/ Exercise 1
 
+getEmployee(1)
+        .then(employee => console.log(employee))
+        .catch(error => console.error(error));
